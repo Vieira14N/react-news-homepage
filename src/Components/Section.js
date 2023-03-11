354ff600757f6c512a851b1177f5cddf2d6aed5e
Vieira14N <ianvieira0411@gmail.com>
2023-03-11 15:54:37 -0300

    feat: main section criada e em andamento

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import "../styles/home.css"
 
-export default function () {
+export default function Section () {
   return (
-    <>
-        
-    </>
+    <section className='main'>
+        <div className='main__cover'></div>
+        <div className='main__title'></div>
+        <div className='main__text'></div>
+        <div className='main__sidenav'></div>
+    </section>
   )
 }
